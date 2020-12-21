ff701924293f25742f6e9dcbce2a3489ad9d03a0
Richard Shaw <richardmorganshaw@gmail.com>
2020-12-21 14:16:59 -0800

    created service worker

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('budget-cache-v1')
+      .then(cache => {
+        console.log('Opened cache')
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/style.css',
+          '/icons/icon-512x512.png'
+          '/icons/icon-192x192.png'
+        ])
+      })
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(err => {
+      return caches.match(event.request).then(res => {
+        if (res) {
+          return res
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/')
+        }
+      })
+    })
+  )
+})
